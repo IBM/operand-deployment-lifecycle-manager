1b02d1a5c653f067dd89c9e0b1d7f026aeb886da
Jiaming Hu <horis233@gmail.com>
2020-02-14 10:14:13 -0500

    add unit test for config (#68)

---
+++ pkg/controller/metaoperatorconfig/metaoperatorconfig_controller_test.go
@@ -0,0 +1,109 @@
+//
+// Copyright 2020 IBM Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package metaoperatorconfig
+
+import (
+	"testing"
+
+	v1alpha1 "github.com/IBM/meta-operator/pkg/apis/operator/v1alpha1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+)
+
+// TestConfigConfig runs ReconcileMetaOperatorConfig.Reconcile() against a
+// fake client that tracks a MetaOperatorConfig object.
+func TestConfigController(t *testing.T) {
+	// Set the logger to development mode for verbose logs.
+	logf.SetLogger(logf.ZapLogger(true))
+
+	var (
+		name      = "meta-operator-config"
+		namespace = "common-service"
+	)
+	// Mock request to simulate Reconcile() being called on an event for a
+	// watched resource .
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+
+	// A metaoperatorconfig resource with metadata and spec.
+	config := &v1alpha1.MetaOperatorConfig{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: v1alpha1.MetaOperatorConfigSpec{
+			Services: []v1alpha1.ConfigService{
+				{
+					Name: "jenkins",
+					Spec: map[string]runtime.RawExtension{
+						"jenkins": {Raw: []byte(`{"service":{"port": 8081}}`)},
+					},
+				},
+				{
+					Name: "etcd",
+					Spec: map[string]runtime.RawExtension{
+						"etcdCluster": {Raw: []byte(`{"size": 1}`)},
+					},
+				},
+			},
+		},
+	}
+	// Objects to track in the fake client.
+	objs := []runtime.Object{
+		config,
+	}
+
+	// Register operator types with the runtime scheme.
+	s := scheme.Scheme
+	s.AddKnownTypes(v1alpha1.SchemeGroupVersion, config)
+	// Create a fake client to mock API calls.
+	cl := fake.NewFakeClient(objs...)
+	// Create a ReconcileMetaOperatorConfig object with the scheme and fake client.
+	r := &ReconcileMetaOperatorConfig{client: cl, scheme: s}
+
+	res, err := r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+	// Check the result of reconciliation to make sure it has the desired state.
+	if res.Requeue {
+		t.Error("reconcile requeue which is not expected")
+	}
+
+	// Create a fake client to mock instance not found.
+	cl = fake.NewFakeClient()
+	// Create a ReconcileMetaOperatorConfig object with the scheme and fake client.
+	r = &ReconcileMetaOperatorConfig{client: cl, scheme: s}
+
+	res, err = r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+	// Check the result of reconciliation to make sure it has the desired state.
+	if res.Requeue {
+		t.Error("reconcile requeue which is not expected")
+	}
+}
