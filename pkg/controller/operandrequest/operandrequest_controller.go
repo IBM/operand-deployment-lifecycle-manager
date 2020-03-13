eff62a7bddc8c643d7f87fea48626d6ae292d135
Jiaming Hu <horis233@gmail.com>
2020-03-12 21:42:41 -0400

    Adjust logs level (#209)
    * adjust log level
    
    * code generate
    
    * Update wait for information to V2

--- pkg/controller/operandrequest/operandrequest_controller.go
+++ pkg/controller/operandrequest/operandrequest_controller.go
@@ -230,7 +230,7 @@ func (r *ReconcileOperandRequest) Reconcile(request reconcile.Request) (reconcil
 
 	// Check if all csv deploy successed
 	if requestInstance.Status.Phase != operatorv1alpha1.ClusterPhaseRunning {
-		klog.V(4).Info("Waiting for all the operands deploy successed")
+		klog.V(2).Info("Waiting for all the operands deploy successed")
 		return reconcile.Result{RequeueAfter: 5}, nil
 	}
 
@@ -238,7 +238,7 @@ func (r *ReconcileOperandRequest) Reconcile(request reconcile.Request) (reconcil
 }
 
 func (r *ReconcileOperandRequest) waitForInstallPlan(requestInstance *operatorv1alpha1.OperandRequest, reconcileReq reconcile.Request) error {
-	klog.V(4).Info("Waiting for subscriptions to be ready ...")
+	klog.V(2).Info("Waiting for subscriptions to be ready ...")
 
 	subs := make(map[string]string)
 	err := wait.PollImmediate(time.Second*20, time.Minute*10, func() (bool, error) {
@@ -284,7 +284,7 @@ func (r *ReconcileOperandRequest) waitForInstallPlan(requestInstance *operatorv1
 						subs[found.ObjectMeta.Name] = "Ready"
 					} else {
 						// Subscription existing and not managed by OperandRequest controller
-						klog.V(4).Info("Subscription has created by other user, ignore update/delete it. ", "Subscription.Namespace: ", found.Namespace, "Subscription.Name: ", found.Name)
+						klog.V(3).Info("Subscription has created by other user, ignore update/delete it. ", "Subscription.Namespace: ", found.Namespace, "Subscription.Name: ", found.Name)
 					}
 				}
 			}
@@ -292,7 +292,7 @@ func (r *ReconcileOperandRequest) waitForInstallPlan(requestInstance *operatorv1
 		return ready, nil
 	})
 	for sub, state := range subs {
-		klog.V(3).Info("Subscription: " + sub + ", state: " + state)
+		klog.V(2).Info("Subscription: " + sub + ", state: " + state)
 	}
 	if err != nil {
 		return err
