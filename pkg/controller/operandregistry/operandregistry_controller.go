9c63ede7093eb3350b8e9908e667a60708c5687f
Jiaming Hu <Jiaming.Hu@ibm.com>
2020-03-17 14:04:10 -0400

    Doesn't create default config and registry instance

--- pkg/controller/operandregistry/operandregistry_controller.go
+++ pkg/controller/operandregistry/operandregistry_controller.go
@@ -66,11 +66,14 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	if err != nil {
 		return err
 	}
-
-	deployDirectory := os.Getenv("DEPLOY_DIR")
-	klog.V(2).Info("Initializing default operandregistry instance")
-	if err = util.InitInstance(deployDirectory+"/operator.ibm.com_v1alpha1_operandregistry_cr.yaml", mgr); err != nil {
-		klog.Error("Error creating CR, please create it manually: ", err)
+	
+	deployInstance := os.Getenv("DEPLOY_INSTANCE")
+	if deployInstance == "TRUE" {
+		deployDirectory := os.Getenv("DEPLOY_DIR")
+		klog.V(2).Info("Initializing default operandregistry instance")
+		if err = util.InitInstance(deployDirectory+"/operator.ibm.com_v1alpha1_operandregistry_cr.yaml", mgr); err != nil {
+			klog.Error("Error creating CR, please create it manually: ", err)
+		}
 	}
 
 	return nil
