d217826468ebe236d238c151c2e881bc22d2198b
Jiaming Hu <Jiaming.Hu@ibm.com>
2020-03-19 14:28:32 -0400

    fix lint

--- pkg/controller/operandregistry/operandregistry_controller.go
+++ pkg/controller/operandregistry/operandregistry_controller.go
@@ -71,7 +71,7 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	klog.V(2).Info("Initializing default operandregistry instance")
 	if err = util.InitInstance(deployDirectory+"/operator.ibm.com_v1alpha1_operandregistry_cr.yaml", mgr); err != nil {
 		klog.Error("Error creating CR, please create it manually: ", err)
-	}	
+	}
 
 	return nil
 }
