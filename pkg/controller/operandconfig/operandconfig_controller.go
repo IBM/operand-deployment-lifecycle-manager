8564861dfb6cee657a972c306cc15036c658969d
Danielxlee <lixinxa@cn.ibm.com>
2020-04-13 17:57:32 +0800

    Support reconcile when config change

--- pkg/controller/operandconfig/operandconfig_controller.go
+++ pkg/controller/operandconfig/operandconfig_controller.go
@@ -93,8 +93,6 @@ type ReconcileOperandConfig struct {
 
 // Reconcile reads that state of the cluster for a OperandConfig object and makes changes based on the state read
 // and what is in the OperandConfig.Spec
-// TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
-// a Pod as an example
 // Note:
 // The Controller will requeue the Request to be processed again if the returned error is non-nil or
 // Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
@@ -111,5 +109,33 @@ func (r *ReconcileOperandConfig) Reconcile(request reconcile.Request) (reconcile
 	if err := r.client.Status().Update(context.TODO(), instance); err != nil {
 		return reconcile.Result{}, err
 	}
+
+	if err := r.updateOperandRequestStatus(request); err != nil {
+		return reconcile.Result{}, err
+	}
+
 	return reconcile.Result{}, nil
+}
+
+// updateOperandRequestStatus update the cluster phase of OperandRequest
+// When the config changed, query all the OperandRequests that use it, and then
+// update these OperandRequest's Phase to Updating, to trigger reconcile of these OperandRequests.
+func (r *ReconcileOperandConfig) updateOperandRequestStatus(request reconcile.Request) error {
+	// Get the requests related with current config
+	requestList := &operatorv1alpha1.OperandRequestList{}
+
+	opts := []client.ListOption{
+		client.MatchingLabels(map[string]string{request.Namespace + "." + request.Name + "/config": "true"}),
+	}
+	if err := r.client.List(context.TODO(), requestList, opts...); err != nil {
+		return err
+	}
+
+	for _, req := range requestList.Items {
+		req.SetUpdatingClusterPhase()
+		if err := r.client.Status().Update(context.TODO(), &req); err != nil {
+			return err
+		}
+	}
+	return nil
 }
