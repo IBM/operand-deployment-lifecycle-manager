e107e3cbe37eaa57b1ec6a7a0064ced7c425b2af
Jiaming Hu <Jiaming.Hu@ibm.com>
2022-04-27 20:50:18 -0400

    update kustomize version

--- common/Makefile.common.mk
+++ common/Makefile.common.mk
@@ -89,7 +89,7 @@ controller-gen: ## Download controller-gen locally if necessary.
 
 KUSTOMIZE ?= $(shell pwd)/common/bin/kustomize
 kustomize: ## Download kustomize locally if necessary.
-	$(call go-get-tool,$(KUSTOMIZE),sigs.k8s.io/kustomize/kustomize/v3@v4.5.4)
+	$(call go-get-tool,$(KUSTOMIZE),sigs.k8s.io/kustomize/kustomize/v4@v4.5.4)
 
 KIND ?= $(shell pwd)/common/bin/kind
 kind: ## Download kind locally if necessary.
