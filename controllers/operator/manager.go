fe3794d2249c82798836c3c513aa67dba891161a
YuChen <yuchen.shen@mail.utoronto.ca>
2023-08-23 18:00:58 -0700

    fix odlm permission issue due to missing opt ns
    Signed-off-by: YuChen <yuchen.shen@mail.utoronto.ca>

--- controllers/operator/manager.go
+++ controllers/operator/manager.go
@@ -87,7 +87,7 @@ func (m *ODLMOperator) GetOperandRegistry(ctx context.Context, key types.Namespa
 			reg.Spec.Operators[i].Namespace = key.Namespace
 		}
 		if o.SourceName == "" || o.SourceNamespace == "" {
-			catalogSourceName, catalogSourceNs, err := m.GetCatalogSourceFromPackage(ctx, o.PackageName, o.Namespace, o.Channel, key.Namespace, excludedCatalogSources)
+			catalogSourceName, catalogSourceNs, err := m.GetCatalogSourceFromPackage(ctx, o.PackageName, reg.Spec.Operators[i].Namespace, o.Channel, key.Namespace, excludedCatalogSources)
 			if err != nil {
 				return nil, err
 			}
