65c97b91cda879a2c03bdaca020efd48910a137d
YuChen <yuchen.shen@mail.utoronto.ca>
2023-11-27 09:59:27 -0800

    split the line to reduce its length
    Signed-off-by: YuChen <yuchen.shen@mail.utoronto.ca>

--- controllers/operandbindinfo/operandbindinfo_controller.go
+++ controllers/operandbindinfo/operandbindinfo_controller.go
@@ -799,9 +799,16 @@ func ensureLabelsForConfigMap(cm *corev1.ConfigMap, labels map[string]string) {
 }
 
 func compareSecret(secret *corev1.Secret, existingSecret *corev1.Secret) (needUpdate bool) {
-	return !equality.Semantic.DeepEqual(secret.GetLabels(), existingSecret.GetLabels()) || !equality.Semantic.DeepEqual(secret.Type, existingSecret.Type) || !equality.Semantic.DeepEqual(secret.Data, existingSecret.Data) || !equality.Semantic.DeepEqual(secret.StringData, existingSecret.StringData) || !equality.Semantic.DeepEqual(secret.GetOwnerReferences(), existingSecret.GetOwnerReferences())
+	return !equality.Semantic.DeepEqual(secret.GetLabels(), existingSecret.GetLabels()) ||
+		!equality.Semantic.DeepEqual(secret.Type, existingSecret.Type) ||
+		!equality.Semantic.DeepEqual(secret.Data, existingSecret.Data) ||
+		!equality.Semantic.DeepEqual(secret.StringData, existingSecret.StringData) ||
+		!equality.Semantic.DeepEqual(secret.GetOwnerReferences(), existingSecret.GetOwnerReferences())
 }
 
 func compareConfigMap(configMap *corev1.ConfigMap, existingConfigMap *corev1.ConfigMap) (needUpdate bool) {
-	return !equality.Semantic.DeepEqual(configMap.GetLabels(), existingConfigMap.GetLabels()) || !equality.Semantic.DeepEqual(configMap.Data, existingConfigMap.Data) || !equality.Semantic.DeepEqual(configMap.BinaryData, existingConfigMap.BinaryData) || !equality.Semantic.DeepEqual(configMap.GetOwnerReferences(), existingConfigMap.GetOwnerReferences())
+	return !equality.Semantic.DeepEqual(configMap.GetLabels(), existingConfigMap.GetLabels()) ||
+		!equality.Semantic.DeepEqual(configMap.Data, existingConfigMap.Data) ||
+		!equality.Semantic.DeepEqual(configMap.BinaryData, existingConfigMap.BinaryData) ||
+		!equality.Semantic.DeepEqual(configMap.GetOwnerReferences(), existingConfigMap.GetOwnerReferences())
 }
