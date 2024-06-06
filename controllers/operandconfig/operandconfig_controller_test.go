d4a3db0f89b71d3ce0569ae5b51835f5ed2eb3cc
Daniel Fan <fanyuchensx@gmail.com>
2024-06-06 11:10:12 -0400

    Find CSVs by label for an operator, to ensure deleting all the CSVs for a specific subscription (#1055)
    * Find CSVs by label for an operator, to ensure deleting all the CSVs for a specific subscription
    
    Signed-off-by: Daniel Fan <fanyuchensx@gmail.com>
    
    * Update test case
    
    Signed-off-by: Daniel Fan <fanyuchensx@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Fan <fanyuchensx@gmail.com>

--- controllers/operandconfig/operandconfig_controller_test.go
+++ controllers/operandconfig/operandconfig_controller_test.go
@@ -121,7 +121,7 @@ var _ = Describe("OperandConfig controller", func() {
 			}, timeout, interval).Should(Succeed())
 
 			By("Creating and Setting status of the ClusterServiceVersions")
-			jaegerCSV := testutil.ClusterServiceVersion("jaeger-csv.v0.0.1", operatorNamespaceName, testutil.JaegerExample)
+			jaegerCSV := testutil.ClusterServiceVersion("jaeger-csv.v0.0.1", "jaeger", operatorNamespaceName, testutil.JaegerExample)
 			Expect(k8sClient.Create(ctx, jaegerCSV)).Should(Succeed())
 			Eventually(func() error {
 				k8sClient.Get(ctx, types.NamespacedName{Name: "jaeger-csv.v0.0.1", Namespace: operatorNamespaceName}, jaegerCSV)
@@ -129,7 +129,7 @@ var _ = Describe("OperandConfig controller", func() {
 				return k8sClient.Status().Update(ctx, jaegerCSV)
 			}, timeout, interval).Should(Succeed())
 
-			mongodbCSV := testutil.ClusterServiceVersion("mongodb-atlas-kubernetes-csv.v0.0.1", operatorNamespaceName, testutil.MongodbExample)
+			mongodbCSV := testutil.ClusterServiceVersion("mongodb-atlas-kubernetes-csv.v0.0.1", "mongodb-atlas-kubernetes", operatorNamespaceName, testutil.MongodbExample)
 			Expect(k8sClient.Create(ctx, mongodbCSV)).Should(Succeed())
 			Eventually(func() error {
 				k8sClient.Get(ctx, types.NamespacedName{Name: "mongodb-atlas-kubernetes-csv.v0.0.1", Namespace: operatorNamespaceName}, mongodbCSV)
