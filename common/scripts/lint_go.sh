3fc3bbb1bb3bef6ddd326eb7f51aa674c4eff25f
Daniel Fan <fanyuchensx@gmail.com>
2025-05-14 08:21:58 -0700

    Increase golangci-lint timeout
    Signed-off-by: Daniel Fan <fanyuchensx@gmail.com>

--- common/scripts/lint_go.sh
+++ common/scripts/lint_go.sh
@@ -15,4 +15,4 @@
 # limitations under the License.
 #
 
-GOGC=25 golangci-lint run -c ./common/config/.golangci.yml --timeout=480s
+GOGC=25 golangci-lint run -c ./common/config/.golangci.yml --timeout=600s
