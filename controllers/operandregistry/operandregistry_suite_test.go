da784d10b3fb939e856b8d72e98e0e1a48a7afe6
Allen Li <liyuchen223@gmail.com>
2025-05-30 12:28:11 -0700

    update ginkgo version
    Signed-off-by: Allen Li <liyuchen223@gmail.com>

--- controllers/operandregistry/operandregistry_suite_test.go
+++ controllers/operandregistry/operandregistry_suite_test.go
@@ -23,7 +23,7 @@ import (
 	"time"
 
 	jaegerv1 "github.com/jaegertracing/jaeger-operator/apis/v1"
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
 	olmv1 "github.com/operator-framework/api/pkg/operators/v1"
