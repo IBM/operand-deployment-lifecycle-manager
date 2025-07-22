4dea0b1048dc4601567910d19a677676a55e1940
Allen Li <liyuchen223@gmail.com>
2025-07-21 19:24:32 -0700

    print reg
    Signed-off-by: Allen Li <liyuchen223@gmail.com>

--- test/e2e/helpers_test.go
+++ test/e2e/helpers_test.go
@@ -218,6 +218,7 @@ func waitRegistryStatus(ctx context.Context, expectedPhase v1alpha1.RegistryPhas
 		if reg.Status.Phase != expectedPhase {
 			fmt.Println("    --- Waiting for registry phase " + expectedPhase)
 			fmt.Println("    --- OperandRegistry status: " + reg.Status.Phase)
+			fmt.Println(reg)
 			return false, nil
 		}
 		return true, nil
