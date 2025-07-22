6a9998870999a8b0f1398a49c9adc257e913c49e
Allen Li <liyuchen223@gmail.com>
2025-07-22 13:27:47 -0700

    add operatorconfig
    Signed-off-by: Allen Li <liyuchen223@gmail.com>

--- main.go
+++ main.go
@@ -31,6 +31,7 @@ import (
 	"k8s.io/klog"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	nssv1 "github.com/IBM/ibm-namespace-scope-operator/api/v1"
@@ -79,7 +80,13 @@ func main() {
 			"Enabling this will ensure there is only one active controller manager.")
 	var stepSize = flag.Int("batch-chunk-size", 3, "batch-chunk-size is used to control at most how many subscriptions will be created concurrently")
 
+	opts := zap.Options{
+		Development: true,
+	}
+	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
+
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	options := ctrl.Options{
 		Scheme:                 scheme,
