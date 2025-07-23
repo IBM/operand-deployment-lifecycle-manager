489805e441563f7568c370b2f9612b8deb092d04
Allen Li <liyuchen223@gmail.com>
2025-07-23 07:15:04 -0700

    remove unused log
    Signed-off-by: Allen Li <liyuchen223@gmail.com>

--- test/e2e/helpers_test.go
+++ test/e2e/helpers_test.go
@@ -218,9 +218,6 @@ func waitRegistryStatus(ctx context.Context, expectedPhase v1alpha1.RegistryPhas
 		if reg.Status.Phase != expectedPhase {
 			fmt.Println("    --- Waiting for registry phase " + expectedPhase)
 			fmt.Println("    --- OperandRegistry status: " + reg.Status.Phase)
-			fmt.Println(reg.ObjectMeta)
-			fmt.Println(reg.Spec)
-			fmt.Println(reg.Status)
 			return false, nil
 		}
 		return true, nil
